9ff554539caa1ee9b924cd880843dc326e052fdc
Night-SHANG <59331024+Night-SHANG@users.noreply.github.com>
2026-09-17 13:30:40 +0800

    fix: run EPUB contract tests with Robolectric

--- app/src/test/java/com/arjun/gander/LibraryEpubContractTest.kt
+++ app/src/test/java/com/arjun/gander/LibraryEpubContractTest.kt
@@ -12,7 +12,10 @@ import java.io.File
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class LibraryEpubContractTest {
 
     private lateinit var context: Context
