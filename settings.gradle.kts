c0627479937e4d4f766f6f8efa92698529102d43
Night-SHANG <59331024+Night-SHANG@users.noreply.github.com>
2026-09-18 20:23:02 +0800

    build: add pinned DroidFS source modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,3 +27,6 @@ dependencyResolutionManagement {
 rootProject.name = "Gander"
 include(":app")
 include(":legado-upstream")
+include(":droidfs-upstream")
+include(":droidfs-pdfviewer")
+project(":droidfs-pdfviewer").projectDir = file("third_party/droidfs/libpdfviewer/app")
