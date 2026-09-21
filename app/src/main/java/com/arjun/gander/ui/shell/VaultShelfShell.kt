7830b81c9840fe0916c5c83cc79b92b79c33381c
Night-SHANG <59331024+Night-SHANG@users.noreply.github.com>
2026-09-21 10:35:39 +0800

    fix: use WindowInsets bottom member API

--- app/src/main/java/com/arjun/gander/ui/shell/VaultShelfShell.kt
+++ app/src/main/java/com/arjun/gander/ui/shell/VaultShelfShell.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.getBottom
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
